022c06a6238d263c404a12414b6cbbde4d608bf8
Stefan Brus <stefan1brus@gmail.com>
2014-02-18 01:42:19 +0100

    Make drag-and-paint work
    Fixes #7.
    Click-and-drag painting now works for all nonogram cell states.

--- src/game.js
+++ src/game.js
@@ -24,55 +24,64 @@ var Game = function() {
 
         this.grid.init();
 
+        // initialize 'drag and paint'-functionality
         $('.grid-cell').mousedown(function(event) {
+            var row = game.getCellNumber($(this).parent().attr('id'));
+            var col = game.getCellNumber($(this).attr('id'));
+            var cur_state = game.grid.cells[row][col];
+
             if(event.which === game.EVENT_LEFT_CLICK) {
-                game.fill(this, true);
+                var new_state =
+                    cur_state === CellState.MARKED || cur_state === CellState.EMPTY
+                    ? CellState.FILLED
+                    : CellState.EMPTY;
+
+                game.fill(this, new_state);
+                $('.grid-cell').mouseover(function() {
+                    game.fill(this, new_state);
+                });
             }
             else if(event.which === game.EVENT_RIGHT_CLICK) {
-                game.fill(this, false);
+                var new_state =
+                    cur_state === CellState.EMPTY || cur_state === CellState.FILLED
+                    ? CellState.MARKED
+                    : CellState.EMPTY;
+
+                game.fill(this, new_state);
+                $('.grid-cell').mouseover(function() {
+                    game.fill(this, new_state);
+                });
             }
+        });
+
+        // stop 'drag and paint' when mouse button is released
+        $(document).mouseup(function() {
+            $('.grid-cell').unbind('mouseover');
         });
     }
 
-    this.fill = function(cell, left_click) {
+    this.fill = function(cell, new_state) {
         var row = this.getCellNumber($(cell).parent().attr('id'));
         var col = this.getCellNumber($(cell).attr('id'));
 
-        if(left_click) {
-            switch (this.grid.cells[row][col]) {
-                case CellState.MARKED:
-                    $(cell).empty();
-                    $(cell).css({'background-color': '#000000'});
-                    this.grid.cells[row][col] = CellState.FILLED;
-                    break;
-                case CellState.EMPTY:
-                    $(cell).css('background-color', '#000000');
-                    this.grid.cells[row][col] = CellState.FILLED;
-                    break;
-                case CellState.FILLED:
-                    $(cell).css('background-color', '#FFFFFF');
-                    this.grid.cells[row][col] = CellState.EMPTY;
-                    break;
-                default:
-                    break;
-            }
-        }
-        else {
-            switch (this.grid.cells[row][col]) {
-                case CellState.EMPTY:
-                case CellState.FILLED:
-                    $(cell).css({'background-color': '#FFFFFF'});
-                    $(cell).text('X');
-                    this.grid.cells[row][col] = CellState.MARKED;
-                    break;
-                case CellState.MARKED:
-                    $(cell).css({'background-color': '#FFFFFF'});
-                    $(cell).empty();
-                    this.grid.cells[row][col] = CellState.EMPTY;
-                    break;
-                default:
-                    break;
-            }
+        switch(new_state) {
+            case CellState.MARKED:
+                $(cell).css({'background-color': '#FFFFFF'});
+                $(cell).text('X');
+                this.grid.cells[row][col] = CellState.MARKED;
+                break;
+            case CellState.EMPTY:
+                $(cell).empty();
+                $(cell).css('background-color', '#FFFFFF');
+                this.grid.cells[row][col] = CellState.EMPTY;
+                break;
+            case CellState.FILLED:
+                $(cell).empty();
+                $(cell).css('background-color', '#000000');
+                this.grid.cells[row][col] = CellState.FILLED;
+                break;
+            default:
+                break;
         }
     }
 
